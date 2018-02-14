cb2570fb56c977389fae5cc87f508cd77414adc2
Dan F-M <danfm@nyu.edu>
2018-02-13 17:08:59 -0800

    better variable names and fewer allocations

--- transit_periodogram/transit_periodogram_impl.pyx
+++ transit_periodogram/transit_periodogram_impl.pyx
@@ -2,7 +2,9 @@ import numpy as np
 cimport numpy as np
 
 cimport cython
+
 from libc.math cimport sqrt
+from libc.stdlib cimport malloc, free
 
 DTYPE = np.float64
 ctypedef np.float64_t DTYPE_t
@@ -10,147 +12,183 @@ ctypedef np.float64_t DTYPE_t
 IDTYPE = np.int32
 ctypedef np.int32_t IDTYPE_t
 
+
+cdef double compute_log_like(
+        double flux_in,
+        double flux_out,
+        double ivar_in,
+        double sum_flux2,
+        double sum_flux,
+        double sum_ivar,
+):
+    cdef double arg = flux_in - flux_out
+    cdef double chi2 = sum_flux2 - 2*flux_out*sum_flux
+    chi2 += flux_out*flux_out * sum_ivar
+    chi2 -= arg*arg * ivar_in
+    return -0.5*chi2
+
+
 @cython.cdivision(True)
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cdef fold(int N, # len time
-          double* time_d,
-          double* flux_d,
-          double* ivar_d,
-          double sum_flux2_all,
-          double sum_flux_all,
-          double sum_ivar_all,
-          double period,
-          int K, # len durations
-          int* durations_dbin_d,
-          double d_bin,
-          int oversample,
-          int use_likelihood=0):
+cdef void fold(
+    int N,              # len time
+    double* time,
+    double* flux,
+    double* ivar,
+    double sum_flux2,
+    double sum_flux,
+    double sum_ivar,
+    double period,
+    int K,              # len durations
+    int* durations,     # in units of d_bin
+    double d_bin,
+    int oversample,
+    int use_likelihood,
+    double* mean_flux,  # must be at least n_bins long
+    double* mean_ivar,  # must be at least n_bins long
+
+    # Outputs
+    double* best_objective,
+    double* best_depth,
+    double* best_depth_std,
+    double* best_depth_snr,
+    double* best_log_like,
+    double* best_phase,
+    double* best_duration
+):
 
     cdef int n_bins = int(period / d_bin) + oversample
-    cdef np.ndarray[DTYPE_t] mean_flux = np.zeros(n_bins, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] mean_ivar = np.zeros(n_bins, dtype=DTYPE)
-    cdef double* mean_flux_d = <double*>mean_flux.data
-    cdef double* mean_ivar_d = <double*>mean_ivar.data
+    cdef int ind, n, k
+    cdef double flux_in, flux_out, ivar_in, ivar_out, \
+                depth, depth_std, depth_snr, log_like, objective
 
-    cdef int ind
-    cdef int n
-    cdef int k
+    for n in range(n_bins):
+        mean_flux[n] = 0.0
+        mean_ivar[n] = 0.0
 
     # Bin the data
     for n in range(N):
-        ind = int((time_d[n] % period) / period * n_bins);
-        mean_flux_d[ind] += flux_d[n] * ivar_d[n]
-        mean_ivar_d[ind] += ivar_d[n]
+        ind = int((time[n] % period) / period * n_bins);
+        mean_flux[ind] += flux[n] * ivar[n]
+        mean_ivar[ind] += ivar[n]
 
     # Pad the bins
     for n in range(oversample):
         ind = n_bins-oversample+n
-        mean_flux_d[ind] = mean_flux_d[n]
-        mean_ivar_d[ind] = mean_ivar_d[n]
+        mean_flux[ind] = mean_flux[n]
+        mean_ivar[ind] = mean_ivar[n]
 
     # Cumsum
     for n in range(1, n_bins):
-        mean_flux_d[n] += mean_flux_d[n-1]
-        mean_ivar_d[n] += mean_ivar_d[n-1]
+        mean_flux[n] += mean_flux[n-1]
+        mean_ivar[n] += mean_ivar[n-1]
 
-    cdef double ll
-    cdef double hin
-    cdef double hout
-    cdef double hin_ivar
-    cdef double hout_ivar
-    cdef double depth
-    cdef double depth_std
-    cdef double depth_snr
-    cdef double hout2
-    cdef double best = -np.inf
-    cdef double obj
-
-    cdef double best_depth, best_depth_std, best_ll, best_phase, best_duration
-
+    best_objective[0] = -np.inf
     for n in range(n_bins - oversample):
         for k in range(K):
-            hin = mean_flux_d[n+durations_dbin_d[k]] - mean_flux_d[n]
-            hin_ivar = mean_ivar_d[n+durations_dbin_d[k]] - mean_ivar_d[n]
-            hout = sum_flux_all - hin
-            hout_ivar = sum_ivar_all - hin_ivar
+            flux_in = mean_flux[n+durations[k]] - mean_flux[n]
+            ivar_in = mean_ivar[n+durations[k]] - mean_ivar[n]
+            flux_out = sum_flux - flux_in
+            ivar_out = sum_ivar - ivar_in
 
-            hin /= hin_ivar
-            hout /= hout_ivar
+            flux_in /= ivar_in
+            flux_out /= ivar_out
 
-            depth = hout - hin
             if use_likelihood:
-                hout2 = hout * hout
-                obj = sum_flux2_all - 2*hout*sum_flux_all + hout2*sum_ivar_all
-                obj += ((hin**2-hout2) + 2*depth*hin)*hin_ivar
-                obj *= -0.5
+                objective = compute_log_like(flux_in, flux_out, ivar_in,
+                                             sum_flux2, sum_flux, sum_ivar)
             else:
-                depth_std = sqrt(1.0 / hin_ivar + 1.0 / hout_ivar)
-                obj = depth / depth_std
+                depth = flux_out - flux_in
+                depth_std = sqrt(1.0 / ivar_in + 1.0 / ivar_out)
+                objective = depth / depth_std
 
-            if obj > best:
+            if objective > best_objective[0]:
                 if use_likelihood:
-                    depth_std = sqrt(1.0 / hin_ivar + 1.0 / hout_ivar)
+                    depth = flux_out - flux_in
+                    depth_std = sqrt(1.0 / ivar_in + 1.0 / ivar_out)
                     depth_snr = depth / depth_std
-                    ll = obj
+                    log_like = objective
                 else:
-                    hout2 = hout * hout
-                    ll = sum_flux2_all - 2*hout*sum_flux_all + hout2*sum_ivar_all
-                    ll += ((hin**2-hout2) + 2*depth*hin)*hin_ivar
-                    ll *= -0.5
+                    log_like = compute_log_like(flux_in, flux_out, ivar_in,
+                                          sum_flux2, sum_flux,
+                                          sum_ivar)
 
-                best = obj
-                best_depth = depth
-                best_depth_std = depth_std
-                best_ll = ll
-                best_phase = n * d_bin
-                best_duration = durations_dbin_d[k] * d_bin
-
-    return best, best_depth, best_depth_std, best_ll, best_phase, best_duration
+                best_objective[0] = objective
+                best_depth[0] = depth
+                best_depth_std[0] = depth_std
+                best_depth_snr[0] = depth_snr
+                best_log_like[0] = log_like
+                best_phase[0] = n * d_bin
+                best_duration[0] = durations[k] * d_bin
 
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
 @cython.wraparound(False)
-def transit_periodogram_impl(np.ndarray[DTYPE_t, mode='c'] time,
-                        np.ndarray[DTYPE_t, mode='c'] flux,
-                        np.ndarray[DTYPE_t, mode='c'] flux_ivar,
-                        np.ndarray[DTYPE_t, mode='c'] periods,
-                        np.ndarray[DTYPE_t, mode='c'] durations,
-                        int oversample,
-                        int use_likelihood=0):
+def transit_periodogram_impl(
+        np.ndarray[DTYPE_t, mode='c'] time_array,
+        np.ndarray[DTYPE_t, mode='c'] flux_array,
+        np.ndarray[DTYPE_t, mode='c'] flux_ivar_array,
+        np.ndarray[DTYPE_t, mode='c'] period_array,
+        np.ndarray[DTYPE_t, mode='c'] duration_array,
+        int oversample,
+        int use_likelihood=0):
 
-    cdef double d_bin = max(durations) / oversample
-    cdef double* periods_d = <double*>periods.data
-    cdef np.ndarray[IDTYPE_t] durations_dbin = np.asarray(durations / d_bin,
-                                                          dtype=IDTYPE)
-    cdef double* time_d = <double*>time.data
-    cdef double* flux_d = <double*>flux.data
-    cdef double* ivar_d = <double*>flux_ivar.data
-    cdef int* durations_dbin_d = <int*>durations_dbin.data
-    cdef int N = len(time)
-    cdef int K = len(durations)
+    cdef double d_bin = max(duration_array) / oversample
+    cdef double* periods = <double*>period_array.data
+    cdef np.ndarray[IDTYPE_t] duration_int_array = \
+            np.asarray(duration_array / d_bin, dtype=IDTYPE)
+    cdef double* time = <double*>time_array.data
+    cdef double* flux = <double*>flux_array.data
+    cdef double* ivar = <double*>flux_ivar_array.data
+    cdef int* durations = <int*>duration_int_array.data
+    cdef int N = len(time_array)
+    cdef int K = len(duration_array)
 
     cdef int p
-    cdef int P = len(periods)
+    cdef int P = len(period_array)
 
-    cdef double sum_flux2_all = np.sum(flux * flux * flux_ivar)
-    cdef double sum_flux_all = np.sum(flux * flux_ivar)
-    cdef double sum_ivar_all = np.sum(flux_ivar)
-    cdef np.ndarray[DTYPE_t] periodogram = np.empty(P, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] depths = np.empty(P, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] depths_snr = np.empty(P, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] depths_std = np.empty(P, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] lls = np.empty(P, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] phases = np.empty(P, dtype=DTYPE)
-    cdef np.ndarray[DTYPE_t] best_durations = np.empty(P, dtype=DTYPE)
+    cdef double sum_flux2 = np.sum(flux_array * flux_array * flux_ivar_array)
+    cdef double sum_flux = np.sum(flux_array * flux_ivar_array)
+    cdef double sum_ivar = np.sum(flux_ivar_array)
+
+    cdef np.ndarray[DTYPE_t] out_objective_array = np.empty(P, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t] out_depth_array     = np.empty(P, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t] out_depth_std_array = np.empty(P, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t] out_depth_snr_array = np.empty(P, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t] out_log_like_array  = np.empty(P, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t] out_phase_array     = np.empty(P, dtype=DTYPE)
+    cdef np.ndarray[DTYPE_t] out_duration_array  = np.empty(P, dtype=DTYPE)
+
+    cdef double* out_objective = <double*>out_objective_array.data
+    cdef double* out_depth     = <double*>out_depth_array.data
+    cdef double* out_depth_std = <double*>out_depth_std_array.data
+    cdef double* out_depth_snr = <double*>out_depth_snr_array.data
+    cdef double* out_log_like  = <double*>out_log_like_array.data
+    cdef double* out_phase     = <double*>out_phase_array.data
+    cdef double* out_duration  = <double*>out_duration_array.data
+
+    cdef int max_n_bins = int(np.max(period_array) / d_bin) + oversample
+    cdef double* mean_flux = <double*>malloc(max_n_bins*sizeof(double))
+    if not mean_flux:
+        raise MemoryError()
+    cdef double* mean_ivar = <double*>malloc(max_n_bins*sizeof(double))
+    if not mean_ivar:
+        free(mean_flux)
+        raise MemoryError()
 
     for p in range(P):
-        periodogram[p], depths[p], depths_std[p], lls[p], phases[p], best_durations[p] = \
-                fold(N, time_d, flux_d, ivar_d, sum_flux2_all, sum_flux_all,
-                     sum_ivar_all, periods_d[p], K, durations_dbin_d, d_bin,
-                     oversample, use_likelihood)
-        depths_snr[p] = depths[p] / depths_std[p]
+        fold(N, time, flux, ivar, sum_flux2, sum_flux,
+             sum_ivar, periods[p], K, durations, d_bin,
+             oversample, use_likelihood, mean_flux, mean_ivar,
+             out_objective+p, out_depth+p, out_depth_std+p,
+             out_depth_snr+p, out_log_like+p, out_phase+p, out_duration+p)
 
-    return (periods, periodogram, lls, depths_snr, depths, depths_std,
-            phases, best_durations)
+    free(mean_flux)
+    free(mean_ivar)
+
+    return (
+        period_array, out_objective_array, out_log_like_array,
+        out_depth_snr_array, out_depth_array, out_depth_std_array,
+        out_phase_array, out_duration_array)
